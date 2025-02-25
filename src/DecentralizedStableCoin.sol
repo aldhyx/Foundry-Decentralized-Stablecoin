fb970da8ae2ddefcb86d54c7484e74dd5118c154
aldhyx <aldyzkobf@gmail.com>
2025-02-26 00:27:35 +0800

    fix error naming convention

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -14,32 +14,29 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 Implementation of our stablecoin system
  */
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
-    error DecentralizedStableCoin_MustBeMoreThanZero();
-    error DecentralizedStableCoin_BurnAmountExceedBalance();
-    error DecentralizedStableCoin_NotZeroAddress();
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
 
     constructor() ERC20("DecentralizedStableCoin", "DSC") {}
 
     function burn(uint256 _amount) external override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
         if (_amount <= 0) {
-            revert DecentralizedStableCoin_MustBeMoreThanZero();
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
         }
         if (balance < _amount) {
-            revert DecentralizedStableCoin_BurnAmountExceedBalance();
+            revert DecentralizedStableCoin__BurnAmountExceedBalance();
         }
         super.burn(_amount);
     }
 
-    function mint(
-        address _to,
-        uint256 _amount
-    ) external onlyOwner returns (bool) {
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
         if (_to == address(0)) {
-            revert DecentralizedStableCoin_NotZeroAddress();
+            revert DecentralizedStableCoin__NotZeroAddress();
         }
         if (_amount <= 0) {
-            revert DecentralizedStableCoin_MustBeMoreThanZero();
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
         }
         _mint(_to, _amount);
         return true;
